17e707898303d43bd727b9428858c5803c0e4ac5
Henry Blanchette <henry@monkeyface.com>
2026-03-16 15:24:05 -0400

    setup run for gan_v1

--- examples/gan_v1/critic/initial.py
+++
@@ -1,8 +0,0 @@
-# EVOLVE-BLOCK-START
-
-
-def criticize(input: str) -> str:
-    return "Looks good to me."
-
-
-# EVOLVE-BLOCK-END
--- examples/gan_v1/generator/initial.py
+++
@@ -1,8 +0,0 @@
-# EVOLVE-BLOCK-START
-
-
-def generate() -> str:
-    return "A man walked into a bar. He said 'ouch'."
-
-
-# EVOLVE-BLOCK-END
