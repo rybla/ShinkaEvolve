f0a032303ea7c371d39e541d01f79dd59237098f
Henry Blanchette <henry@monkeyface.com>
2026-03-16 13:42:25 -0400

    gan_v1 template

---
+++ examples/gan_v1/critic/run_evo.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import argparse
+
+import yaml
+
+from shinka.core import ShinkaEvolveRunner, EvolutionConfig
+from shinka.database import DatabaseConfig
+from shinka.launch import LocalJobConfig
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+search_task_sys_msg = """
+TODO: critic instructions
+""".strip()
+
+
+def main(config_path: str):
+    with open(config_path, "r", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+
+    config["evo_config"]["task_sys_msg"] = search_task_sys_msg
+    evo_config = EvolutionConfig(**config["evo_config"])
+    job_config = LocalJobConfig(
+        eval_program_path="evaluate.py",
+        time="00:05:00",
+    )
+    db_config = DatabaseConfig(**config["db_config"])
+
+    runner = ShinkaEvolveRunner(
+        evo_config=evo_config,
+        job_config=job_config,
+        db_config=db_config,
+        max_evaluation_jobs=config.get("max_evaluation_jobs"),
+        max_proposal_jobs=config.get("max_proposal_jobs"),
+        max_db_workers=config.get("max_db_workers"),
+        debug=False,
+        verbose=True,
+    )
+    runner.run()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_path", type=str)
+    args = parser.parse_args()
+    main(args.config_path)
---
+++ examples/gan_v1/generator/initial.py
@@ -0,0 +1,8 @@
+# EVOLVE-BLOCK-START
+
+
+def generate() -> str:
+    return "A man walked into a bar. He said 'ouch'."
+
+
+# EVOLVE-BLOCK-END
